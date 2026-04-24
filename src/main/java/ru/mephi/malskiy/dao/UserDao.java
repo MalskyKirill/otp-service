1c8843f4c20a535f2f454557e29d42545e0c3ab6
Kirill Malskiy <malskykirill@yandex.ru>
2026-04-24 10:53:21 +0300

    добавил userdao и метод создания пользователя в бд

--- src/main/java/ru/mephi/malskiy/dao/UserDao.java
+++ src/main/java/ru/mephi/malskiy/dao/UserDao.java
@@ -1,6 +1,14 @@
 package ru.mephi.malskiy.dao;
 
 import ru.mephi.malskiy.config.Database;
+import ru.mephi.malskiy.model.User;
+import ru.mephi.malskiy.model.enums.UserRole;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.time.LocalDateTime;
 
 public class UserDao {
 
@@ -8,5 +16,41 @@ public class UserDao {
 
     public UserDao(Database database) {
         this.database = database;
+    }
+
+    public User create(User user) {
+        String sql = """
+            INSERT INTO users (login, password_hash, role)
+            VALUES (?, ?, ?)
+            RETURNING id, login, password_hash, role, created_at
+            """;
+
+        try (Connection connection = database.getConnection();
+             PreparedStatement statement = connection.prepareStatement(sql)) {
+
+            statement.setString(1, user.getLogin());
+            statement.setString(2, user.getPasswordHash());
+            statement.setString(3, user.getRole().name());
+
+            try (ResultSet resultSet = statement.executeQuery()){
+                if (resultSet.next()) {
+                    return mapUser(resultSet);
+                }
+            }
+            throw new SQLException("Failed to create user");
+
+        } catch (SQLException e) {
+            throw new RuntimeException("Database error while creating user", e);
+        }
+    }
+
+    private User mapUser(ResultSet resultSet) throws SQLException {
+        Long id = resultSet.getLong("id");
+        String login = resultSet.getString("login");
+        String passwordHash = resultSet.getString("password_hash");
+        UserRole role = UserRole.valueOf(resultSet.getString("role"));
+        LocalDateTime createdAt = resultSet.getObject("created_at", LocalDateTime.class);
+
+        return new User(id, login, passwordHash, role, createdAt);
     }
 }
