5cdc4cf886cc13e5b807e90f5526fabb7ae09825
Kirill Malskiy <malskykirill@yandex.ru>
2026-05-01 23:12:26 +0300

    добавил FileNotificationServiceImpl

---
+++ src/main/java/ru/mephi/malskiy/service/FileNotificationServiceImpl.java
@@ -0,0 +1,41 @@
+package ru.mephi.malskiy.service;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
+import java.time.LocalDateTime;
+
+public class FileNotificationServiceImpl implements NotificationService{
+    private static final Logger logger = LoggerFactory.getLogger(FileNotificationServiceImpl.class);
+    private final Path file;
+
+    public FileNotificationServiceImpl(Path file) {
+        this.file = Path.of("otp_codes.txt");
+    }
+
+    @Override
+    public void sendCode(String destination, int code) {
+
+        String line = String.format(
+            "[%s] destination=%s, code=%s%n",
+            LocalDateTime.now(),
+            destination,
+            code
+        );
+
+        try {
+            Files.writeString(
+                file, line, StandardOpenOption.CREATE, StandardOpenOption.APPEND
+            );
+
+            logger.info("OTP code written to file {}", file.toAbsolutePath());
+        } catch (IOException e) {
+            throw new RuntimeException("Failed to write OTP code to file", e);
+        }
+    }
+}
